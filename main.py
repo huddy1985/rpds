003c47a64cf5887a439748d7ea920395aba22e83
Xuguo Wang <huddy1985@gmail.com>
2017-12-16 18:31:31 +0800

    modify architecture

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import gprs
+import gprs.gprs as gprs
 
 if __name__ == "__main__":
     gprs = gprs.gprs()
     gprs.init('/dev/ttyUSB0', 115200, 8, 'N', 1, 1)
     gprs.test()
-
-    gprs.getSignalStrength()
+    gprs.msleep(300)
+    gprs.setWorkMode(0)
