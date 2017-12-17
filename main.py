65b6922c114507efb6a04cd0427b12e9430a32f1
Xuguo Wang <huddy1985@gmail.com>
2017-12-17 13:00:51 +0800

    add operation to main.py

--- main.py
+++ main.py
@@ -4,5 +4,26 @@ if __name__ == "__main__":
     gprs = gprs.gprs()
     gprs.init('/dev/ttyUSB0', 115200, 8, 'N', 1, 1)
     gprs.test()
+    # gprs.getIP()
     gprs.msleep(300)
-    gprs.setWorkMode(0)
+    # gprs.getDTU()
+    gprs.setNetMode(1)
+    gprs.msleep(300)
+    # gprs.setNetAT(1)
+    # gprs.msleep(300)
+    # gprs.getDTU()
+    # gprs.msleep(300)
+    # gprs.setConnection(1, 'TCP', '107.170.65.195', 8001, 1)
+    # gprs.msleep(300)
+    # gprs.getDTU()
+    # gprs.conn2('TCP', '107.170.65.195', '8001')
+    gprs.msleep(200)
+    # gprs.getNetStatus()
+    # gprs.msleep(200)
+    gprs.sendData('test')
+    gprs.msleep(10000)
+    # gprs.closeConn()
+
+
+
+
